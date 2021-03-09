1ed531867d345207e0bcb94b833f1c9f1a7497c7
IgnacioMazuran <ignaciomazuran12@gmail.com>
2021-03-09 13:16:55 -0300

    creacion del proyecto

---
+++ src/homepage.component.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const HomePage = () => (
+    <div className='homepage'>
+        <div className='menu-directorio'>
+            <div className='menu-item'>
+                <div className='contenido'>
+                    <h1 className='titulo'>Gorras</h1>
+                    <span className='subtitulo'>Comprar Ahora</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='contenido'>
+                    <h1 className='titulo'>Zapatillas</h1>
+                    <span className='subtitulo'>Comprar Ahora</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='contenido'>
+                    <h1 className='titulo'>Remeras</h1>
+                    <span className='subtitulo'>Comprar Ahora</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='contenido'>
+                    <h1 className='titulo'>Mujeres</h1>
+                    <span className='subtitulo'>Comprar Ahora</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='contenido'>
+                    <h1 className='titulo'>Hombres</h1>
+                    <span className='subtitulo'>Comprar Ahora</span>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+export default HomePage;
