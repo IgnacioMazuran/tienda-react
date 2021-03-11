cc37609a064453bd806877be81fed8599867f3f2
IgnacioMazuran <ignaciomazuran12@gmail.com>
2021-03-11 18:21:23 -0300

    add shop page

--- src/App.js
+++ src/App.js
@@ -3,19 +3,15 @@ import HomePage from './pages/homepage/homepage.component';
 import './pages/homepage/homepage.styles.scss';
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component.jsx';
 
-const PagGorras = () => (
-  <div>
-    <h1>Gorras</h1>
-  </div>
-);
 
 function App() {
   return (
     <div>
       <Switch>
       <Route exact path='/' component={HomePage}/>
-      <Route path='/gorras' component={PagGorras}/>
+      <Route path='/shop' component={ShopPage}/>
       </Switch>
       
     </div>
