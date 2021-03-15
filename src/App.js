7f083555e3ced1f887c3e1d47023bca703193e99
IgnacioMazuran <ignaciomazuran12@gmail.com>
2021-03-15 20:20:17 -0300

    changed styles sign in w google

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import './pages/homepage/homepage.styles.scss';
@@ -31,9 +32,9 @@ class App extends React.Component {
   }
 
   render(){
-    return (
+    return(
       <div>
-        <Header/>
+        <Header currentUser={this.state.currentUser} />
         <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route path='/shop' component={ShopPage}/>
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,17 +1,33 @@
-import React from 'react';
-import './header.styles.scss'
-import { Link } from 'react-router-dom';
-import { ReactComponent as Logo } from '../../assets/tienda.svg';
-const Header = () => (
-    <div className='header'>
-        <Link className='logo-container' to='/'>
-            <Logo className='logo'/>
-        </Link>
-        <div className='options'>
-            <Link className='option' to='/shop'> Tienda</Link>
-            <Link className='option' to='/shop'> Contacto</Link>
-            
+import React from "react";
+import "./header.styles.scss";
+import { Link } from "react-router-dom";
+import { ReactComponent as Logo } from "../../assets/tienda.svg";
+import { auth } from "../../firebase/firebase.utils";
+
+const Header = ({ currentUser }) => (
+  <div className="header">
+    <Link className="logo-container" to="/">
+      <Logo className="logo" />
+    </Link>
+    <div className="options">
+      <Link className="option" to="/shop">
+        {" "}
+        Tienda
+      </Link>
+      <Link className="option" to="/shop">
+        {" "}
+        Contacto
+      </Link>
+      {currentUser ? (
+        <div className="option" onClick={() => auth.signOut()}>
+          Cerrar Sesión
         </div>
+      ) : (
+        <Link className="option" to="/signin">
+          Iniciar Sesión
+        </Link>
+      )}
     </div>
-)
+  </div>
+);
 export default Header;
