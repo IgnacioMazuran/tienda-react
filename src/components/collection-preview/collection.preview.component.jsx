0a150cfac3549361ad6da21c1f220830a9c6a928
Ignacio Mazuran <ignaciomazuran12@gmail.com>
2021-04-16 12:11:45 -0300

    directorio movido a reducer

--- src/components/collection-preview/collection.preview.component.jsx
+++ src/components/collection-preview/collection.preview.component.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import './collection.preview.styles.scss';
 import CollectionItem from '../collection-item/collection-item.component';
-const CollectionPreview = ({titulo,items}) => (
+const CollectionPreview = ({ titulo, items }) => (
     <div className='collection-preview'>
-        <h1 className='titulo'>{titulo.toUpperCase()}</h1>
-        <div className='preview'>
-            {
-                items.filter((item, idx) => idx < 4 ).map((item) => (
-                    <CollectionItem key={item.id} item={item}/>
-                ))
-            }
-        </div>
+      <h1 className='title'>{titulo.toUpperCase()}</h1>
+      <div className='preview'>
+        {items
+          .filter((item, idx) => idx < 4)
+          .map(item => (
+            <CollectionItem key={item.id} item={item} />
+          ))}
+      </div>
     </div>
-)
+  );
 export default CollectionPreview;
